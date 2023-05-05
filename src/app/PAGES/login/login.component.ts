4672b941881856f7bd54500305485afe1f0e25bc
Achille DAVID <davidachille18@gmail.com>
2023-05-05 12:31:54 +0200

    Ajout d'un bouton pour afficher/cacher le mot de passe

--- src/app/PAGES/login/login.component.ts
+++ src/app/PAGES/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
-  invalid_informations: boolean = false;
+  public invalid_informations: boolean = false;
+  public password_visibility: boolean = false;
+
+  constructor() { }
+
+  getPasswordType() {
+    return this.password_visibility ? 'text' : 'password';
+  }
+
+  togglePasswordVisibility() {
+    this.password_visibility = !this.password_visibility;
+  }
 }
