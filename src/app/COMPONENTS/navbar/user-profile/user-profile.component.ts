f68b935cf652327c6422a50c85df5cf3c77dbb59
Achille DAVID <davidachille18@gmail.com>
2023-05-06 11:40:02 +0200

    Ajout de la modal pour le profil utilisateur

--- src/app/COMPONENTS/navbar/user-profile/user-profile.component.ts
+++ src/app/COMPONENTS/navbar/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
+import { ConnectionService } from './../../../SERVICES/connection.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'user-profile',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
+  public opened: boolean = false;
 
+  constructor(private router: Router, private connectionService: ConnectionService) {
+    this.router.events.subscribe((event) => {
+      if (this.opened) this.closeProfileModal();
+    });
+  }
+
+  public toggleProfileModal(): void {
+    this.opened = !this.opened;
+  }
+
+  public closeProfileModal(): void {
+    this.opened = false;
+  }
+
+  public logout(): void {
+    this.connectionService.logout();
+  }
 }
