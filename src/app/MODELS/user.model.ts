c90349915f39bbda4d1a1e3c70f0844acd255f80
Achille DAVID <davidachille18@gmail.com>
2023-05-07 18:25:29 +0200

    Connexion au site

--- src/app/MODELS/user.model.ts
+++ src/app/MODELS/user.model.ts
@@ -2,11 +2,20 @@ export interface User {
     user_id: string;
     username: string;
     password: string;
+<<<<<<< Updated upstream
     email?: string;
     first_name?: string;
     last_name?: string;
     profile_picture?: string;
     created_at?: string;
     updated_at?: string;
+=======
+    email: string;
+    first_name: string;
+    last_name: string;
+    picture_id: string;
+    created_at: Date;
+    updated_at: Date;
+>>>>>>> Stashed changes
     favorite_characters?: Array<string>;
 }
--- src/app/SERVICES/connection.service.ts
+++ src/app/SERVICES/connection.service.ts
@@ -1,21 +1,36 @@
+import { BackendService } from './../BACKEND/backend.service';
 import { TokenService } from './token.service';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class ConnectionService {
-    constructor(public tokenService: TokenService, private router: Router) { }
+    constructor(public tokenService: TokenService, private router: Router, private backend: BackendService) { }
 
-    public login(username: string, password: string) {
-        const user = {
-            username: username,
-            password: password,
-        };
-        this.tokenService.setToken(JSON.stringify(user));
-        this.router.navigate(['/home']);
+    public login(username: string, password: string): Observable<any> {
+        return Observable.create((observer: Observer<any>) => {
+            this.backend.postWithParams('auth/signin', { username: username, password: password })
+                .then((response) => {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        throw new Error('Error while logging in');
+                    }
+                })
+                .then((response) => {
+                    this.tokenService.setToken(response.jwt);
+                    this.router.navigate(['/home']);
+                    observer.next(response);
+                    observer.complete();
+                })
+                .catch((error) => {
+                    observer.error(error);
+                });
+        });
     }
 
     public logout() {
