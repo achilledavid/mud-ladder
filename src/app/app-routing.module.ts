023a3a5a45bd16e7e03b633fbbd97135d44c4aa0
Achille DAVID <davidachille18@gmail.com>
2023-05-07 14:25:50 +0200

    Gestion des mauvaises routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { LoginComponent } from './PAGES/login/login.component';
 import { SignUpComponent } from './PAGES/sign-up/sign-up.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
